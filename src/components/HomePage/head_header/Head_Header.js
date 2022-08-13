f12d492da5acda1b4a3f09fd5e60844de7b12213
Diganta165 <mdsrdiganta@gmail.com>
2022-08-13 18:07:05 +0600

    sidebar open close complete

--- src/components/HomePage/head_header/Head_Header.js
+++ src/components/HomePage/head_header/Head_Header.js
@@ -23,19 +23,21 @@ const Head_Header = () => {
     // // closeSidebarOnOuterClick();
 
     let [isOpen, setIsOpen] = useState(false);
-    let sidebarRef = useRef();
-    useEffect(()=>{
-        let sidebarHandler = (event)=>{
-            if(!sidebarRef.current.contains(event.target)){
-                setIsOpen(false);
-            }
+    // let sidebarRef = useRef();
+    // useEffect(()=>{
+    //     let sidebarHandler = (event)=>{
+    //         if(!sidebarRef.current.contains(event.target)){
+    //             setIsOpen(false);
+    //         }
             
-        };
-        document.addEventListener("click", sidebarHandler)
-        return ()=>{
-            document.removeEventListener("click", sidebarHandler)
-        }
-    });
+    //     };
+    //     document.addEventListener("click", sidebarHandler)
+    //     return ()=>{
+    //         document.removeEventListener("click", sidebarHandler)
+    //     }
+    // });
+    const openSidebar =()=>setIsOpen(true);
+    const closeSidebar = ()=> setIsOpen(false);
     return (
         <div>
             <header className='header_section'>
@@ -50,7 +52,7 @@ const Head_Header = () => {
                         
                     </li>
                     {/* <div className="ham_burger_nav_icon_wrapper" onClick={openSidebar}> */}
-                    <div className="ham_burger_nav_icon_wrapper" >
+                    <div className="ham_burger_nav_icon_wrapper" onClick={openSidebar} >
                             <span className="ham_burger_nav_icon">
                                 <svg className="ham_burger_nav_icon_size" xmlns="http://www.w3.org/2000/svg" width="26.5" height="17.5" viewBox="0 0 26.5 17.5"> <g id="Menu_icon" data-name="Menu icon" transform="translate(-299.25 -36.25)"> <line id="Shape_3" data-name="Shape 3" x2="20" transform="translate(300 53)" fill="none" stroke="#fff" strokelnecap="round" strokeWidth="1.5"></line> <line id="Shape_2" data-name="Shape 2" x2="15" transform="translate(300 45)" fill="none" stroke="#fff" strokelnecap="round" strokeWidth="1.5"></line> <line id="Shape_1" data-name="Shape 1" x2="25" transform="translate(300 37)" fill="none" stroke="#fff" strokelnecap="round" strokeWidth="1.5"></line> </g> 
                                 </svg>
@@ -58,12 +60,13 @@ const Head_Header = () => {
                         </div>
                 </nav>
             </header>
-            <div ref = {sidebarRef} className='sidebar sidebar_popup ' id='sidebar_section'>
+            {/* sidebar sidebar_popup sidebar_show */}
+            <div  className={isOpen === true ? "sidebar sidebar_popup sidebar_show" : "sidebar sidebar_popup"} id='sidebar_section' >
                 {/* <div className='sidebar_container' id='sidebar_container_id' onClick={closeSidebarOnOuterClick}> */}
                 <div className='sidebar_container' id='sidebar_container_id' >
                     <div>
                         {/* <span className="sidebar_close_icon_wrapper" id='sidebar_close_icon' onClick={closeSidebar}> */}
-                        <span className="sidebar_close_icon_wrapper" id='sidebar_close_icon' >
+                        <span className="sidebar_close_icon_wrapper" id='sidebar_close_icon' onClick={closeSidebar}>
                             <svg className="sidebar_close_icon" xmlns="http://www.w3.org/2000/svg" width="17.121" height="17.121" viewBox="0 0 17.121 17.121"> <g id="Menu_icon" data-name="Menu icon" transform="translate(-301.439 -35.439)"> <line id="Shape_3" data-name="Shape 3" x2="21.213" transform="translate(302.5 51.5) rotate(-45)" fill="none" stroke="#fff" strokeLinecap="round" strokeWidth="1.5"></line> <line id="Shape_1" data-name="Shape 1" x2="21.213" transform="translate(302.5 36.5) rotate(45)" fill="none" stroke="#fff" strokeLinecap="round" strokeWidth="1.5"></line> </g> </svg></span>
                     </div>
                     <li className='navigation_link_list sidebar_nav_link_wrapper'>
