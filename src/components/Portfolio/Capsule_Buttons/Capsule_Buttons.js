fbb1ab75d45f8c52100ca1c0da0a0d72a8b04a51
Diganta165 <mdsrdiganta@gmail.com>
2022-12-11 09:42:53 -0800

    portfolio buttons locial error solved

--- src/components/Portfolio/Capsule_Buttons/Capsule_Buttons.js
+++ src/components/Portfolio/Capsule_Buttons/Capsule_Buttons.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 import "./Capsule_buttons.css"
 const Capsule_Buttons = ({button}) => {
-    const [project, setProject] = useState();
+    // let [project, setProject] = useState("");
     const loadPortfolioData = () =>{
         // console.log("o", button.name)
-        setProject(previousValue => previousValue = button.name);
-        console.log("project", project)
+        // const projectName = button.name;
+        // setProject(previousValue => previousValue = projectName);
+        // setProject(projectName);
+        // console.log(project)
+        console.log(button.name)
     }
     return (
         <>
-            <button onClick={loadPortfolioData} className='capsule_button'> {button.name}</button>
+            {/* <button onClick={loadPortfolioData} className='capsule_button'> {button.name}</button> */}
+            <button onClick={loadPortfolioData}  className='capsule_button'> {button.name}</button>
+            
         </>
     );
 };
--- src/components/Portfolio/Portfolio_Title_Capsule/Portfolio_Title_Capsule.js
+++ src/components/Portfolio/Portfolio_Title_Capsule/Portfolio_Title_Capsule.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Portfolio_Title_Capsule.css"
 import Capsule_Buttons from '../Capsule_Buttons/Capsule_Buttons';
 
-const Portfolio_Title_Capsule = () => {
 
-    const capsuleButtons = [
+const Portfolio_Title_Capsule = () => {
+    const capsuleButtonsData = [
         {
             name: "All",
             id:1,
@@ -22,11 +22,35 @@ const Portfolio_Title_Capsule = () => {
         },
     ];
 
+    // const [projectType, setProjectType] = useState("");
+    const [capsuleButtons, setCapsuleButtons] = useState([]);
+
+    // const selectedProjectType = (v) =>{
+    //     // setProjectType(previousValue => previousValue = button);
+    //     // console.log("projectType", button.id)
+    //     console.log("hi", v)
+    // };
+
+    useEffect(()=>{
+        setCapsuleButtons(capsuleButtonsData);
+    }, []);
+
+    
+
     return (
         <div className='capsule_buttons'>
             <div className='capsule_buttons_container'>
                 <div className='capsule_buttons_wrapper'>
-                    {capsuleButtons.map(button => <Capsule_Buttons button = {button} key ={button.id}/>)}
+                    {/* {capsuleButtons.map(button => <Capsule_Buttons onClick = {this.selectedProjectType.bind(this)} button = {button} key ={button.id}/>)} */}
+                    {/* {capsuleButtons.map((button, index) => <Capsule_Buttons onClick = {(button)=>selectedProjectType(button.name)}  button = {button} key ={index} />)} */}
+                    {/* {capsuleButtons.map((button, index) => <Capsule_Buttons onClick = {selectedProjectType}  button = {button} key ={index} />)} */}
+                    {/* {capsuleButtons.map((button, index) => {
+                        console.log(button.name);
+                        <div onClick ={selectedProjectType} key = {index}>button</div>
+                    })} */}
+                    {
+                        capsuleButtons.map((element, index) => <Capsule_Buttons key={index} button ={element}></Capsule_Buttons>)
+                    }
                 </div>
                 
             </div>
