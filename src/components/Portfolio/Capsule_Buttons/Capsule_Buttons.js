e79c17d085f5d414951884ad1f7fd778937d0a5f
Diganta165 <mdsrdiganta@gmail.com>
2022-12-13 08:44:26 -0800

    successfully passed data using state in portfolio buttons

--- src/components/Portfolio/Capsule_Buttons/Capsule_Buttons.js
+++ src/components/Portfolio/Capsule_Buttons/Capsule_Buttons.js
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Capsule_buttons.css"
 const Capsule_Buttons = ({button}) => {
-    // let [project, setProject] = useState("");
+    const [buttonData, setButtonData] = useState({
+        name:"All",
+        id:1,
+        data:[
+
+        ]
+    });
+
+    useEffect(()=>{
+        setButtonData((stateElement)=>{
+            return{...stateElement, name: button.name, id: button.id, data: [button.data]};
+        });
+        console.log("buttonData1",buttonData)
+    },[]);
+
     const loadPortfolioData = () =>{
-        // console.log("o", button.name)
-        // const projectName = button.name;
-        // setProject(previousValue => previousValue = projectName);
-        // setProject(projectName);
-        // console.log(project)
         console.log(button.name)
-    }
+        setButtonData((stateElement)=>{
+            return{...stateElement, name: button.name, id: button.id, data: [button.data]};
+        });
+        console.log("buttonData",buttonData)
+    };
     return (
         <>
             {/* <button onClick={loadPortfolioData} className='capsule_button'> {button.name}</button> */}
